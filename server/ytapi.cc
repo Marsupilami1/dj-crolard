bbe96d4b0d2aee56a14b7c3b6e5f5b0ea73347a8
Marsupilami1 <martin.andrieux59@gmail.com>
2026-06-20 14:33:17 +0200

    ytapi files

---
+++ server/ytapi.cc
@@ -0,0 +1,91 @@
+#include "ytapi.h"
+#include "crow_all.h"
+#include <cpr/cpr.h>
+#include <sstream>
+
+using namespace dj;
+
+YtApi::YtApi(const std::string &api_key) : api_key_(api_key) {}
+
+static std::chrono::seconds ParseISO8601(const std::string &s) {
+    std::chrono::seconds result(0);
+    std::istringstream is(s);
+    uint64_t n = 0;
+
+    is.get(); // P
+    is.get(); // T
+    while (is >> n) {
+        switch (is.get()) {
+        case 'H':
+            result += std::chrono::hours(n);
+            break;
+        case 'M':
+            result += std::chrono::minutes(n);
+            break;
+        case 'S':
+            result += std::chrono::seconds(n);
+            break;
+        }
+    }
+
+    return result;
+}
+
+std::optional<Video> YtApi::Video(const std::string &id) {
+    cpr::Response r = cpr::Get(
+        cpr::Url{"https://www.googleapis.com/youtube/v3/videos"},
+        cpr::Parameters{
+            {"part", "snippet,contentDetails"}, {"id", id}, {"key", api_key_}});
+
+    if (r.status_code != 200) {
+        return std::nullopt;
+    }
+
+    crow::json::rvalue yt_data = crow::json::load(r.text);
+    std::string title = yt_data["items"][0]["snippet"]["title"].s();
+    auto duration =
+        ParseISO8601(yt_data["items"][0]["contentDetails"]["duration"].s());
+
+    dj::Video video{id, title, duration};
+    return video;
+}
+
+std::optional<std::vector<SearchResult>>
+YtApi::Search(const std::string &query) {
+    cpr::Response r =
+        cpr::Get(cpr::Url{"https://www.googleapis.com/youtube/v3/search"},
+                 cpr::Parameters{{"q", query},
+                                 {"maxResults", "5"},
+                                 {"type", "video"},
+                                 {"part", "snippet"},
+                                 {"key", api_key_}});
+
+    if (r.status_code != 200) {
+        return std::nullopt;
+    }
+
+    crow::json::rvalue yt_data = crow::json::load(r.text);
+    std::vector<SearchResult> results;
+
+    //  An "item" has the follwing format {
+    //    "id": {
+    //      "videoId": "some-video-id"
+    //    },
+    //    "snippet": {
+    //      "title": "SOME VIDEO TITLE",
+    //      "thumbnails": {
+    //        "default": {
+    //          "url": "https://i.ytimg.com/vi/some-video-id/default.jpg",
+    //        },
+    //      },
+    //    }
+    //  }
+
+    for (const auto &item : yt_data["items"].lo()) {
+        results.emplace_back(
+            item["id"]["videoId"].s(), item["snippet"]["title"].s(),
+            item["snippet"]["thumbnails"]["default"]["url"].s());
+    }
+
+    return results;
+}
---
+++ server/ytapi.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "state.h"
+#include <string>
+
+namespace dj {
+
+class YtApi {
+  public:
+    YtApi(const std::string &api_key);
+    virtual ~YtApi() = default;
+
+    std::optional<Video> Video(const std::string &id);
+    std::optional<std::vector<SearchResult>> Search(const std::string &query);
+
+  private:
+    std::string api_key_;
+};
+
+} // namespace dj
